1bcdcd38d84bb553478bdc20a15a10c339237121
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-07 22:46:10 +0200

    upgrade deps
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampod/src/chain/blockchain.rs
+++ lampod/src/chain/blockchain.rs
@@ -1,39 +1,41 @@
-use crate::backend::Backend;
+use std::sync::Arc;
+
 use bitcoin::Transaction;
+
 use lightning::chain::chaininterface::{BroadcasterInterface, ConfirmationTarget, FeeEstimator};
 use lightning::chain::chainmonitor::ChainMonitor;
 use lightning::chain::channelmonitor::ChannelMonitor;
 use lightning::chain::keysinterface::InMemorySigner;
 use lightning::chain::Filter;
 use lightning_persister::FilesystemPersister;
-use std::sync::Arc;
 
+use crate::backend::Backend;
 use crate::keys::keys::LampoKeys;
 use crate::persistence::LampoPersistence;
 use crate::utils::logger::LampoLogger;
 
-type LampoChannelMonitor<'a> = ChainMonitor<
+type LampoChannelMonitor = ChainMonitor<
     InMemorySigner,
     Arc<dyn Filter + Send + Sync>,
-    Arc<LampoChainManager<'a>>,
-    Arc<LampoChainManager<'a>>,
+    Arc<LampoChainManager>,
+    Arc<LampoChainManager>,
     Arc<LampoLogger>,
     Arc<FilesystemPersister>,
 >;
 
 #[derive(Clone)]
 /// Lampo FeeEstimator implementation
-struct LampoChainManager<'a> {
-    backend: &'a dyn Backend,
-    persister: Option<&'a LampoPersistence>,
-    keymanager: &'a LampoKeys,
+struct LampoChainManager {
+    backend: Arc<dyn Backend>,
+    persister: Option<Arc<LampoPersistence>>,
+    keymanager: Arc<LampoKeys>,
 }
 
 /// Personal Lampo implementation
-impl<'a> LampoChainManager<'a> {
+impl LampoChainManager {
     /// Create a new instance of LampoFeeEstimator with the specified
     /// Backend.
-    fn new(client: &'a dyn Backend, keys: &'a LampoKeys) -> Self {
+    fn new<'c>(client: Arc<dyn Backend>, keys: Arc<LampoKeys>) -> Self {
         LampoChainManager {
             backend: client,
             persister: None,
@@ -45,24 +47,26 @@ impl<'a> LampoChainManager<'a> {
         &mut self,
         logger: &LampoLogger,
         filter: Arc<dyn Filter + Send + Sync>,
-        persister: &'a LampoPersistence,
-    ) -> Arc<LampoChannelMonitor<'a>> {
-        self.persister = Some(persister);
+        persister: LampoPersistence,
+    ) -> Arc<LampoChannelMonitor> {
+        let per = persister.clone().persister;
+        self.persister = Some(Arc::new(persister));
         Arc::new(ChainMonitor::new(
             Some(filter),
             Arc::new(self.clone()),
             Arc::new(logger.clone()),
             Arc::new(self.clone()),
-            Arc::new(persister.clone().persister),
+            Arc::new(per),
         ))
     }
 
     fn reload(&self) -> Vec<(bitcoin::BlockHash, ChannelMonitor<InMemorySigner>)> {
-        let mut channel_monitors = self
+        let channel_monitors = self
             .persister
+            .clone()
             .unwrap()
             .persister
-            .read_channelmonitors(&self.keymanager.keys_manager)
+            .read_channelmonitors(&self.keymanager.keys_manager, &self.keymanager.keys_manager)
             .unwrap();
         if self.backend.is_lightway() {
             for (_, channel_monitor) in channel_monitors.iter() {
@@ -74,7 +78,7 @@ impl<'a> LampoChainManager<'a> {
 }
 
 /// Rust lightning FeeEstimator implementation
-impl FeeEstimator for LampoChainManager<'_> {
+impl FeeEstimator for LampoChainManager {
     fn get_est_sat_per_1000_weight(&self, confirmation_target: ConfirmationTarget) -> u32 {
         return match confirmation_target {
             ConfirmationTarget::Background => self.backend.fee_rate_estimation(24),
@@ -85,20 +89,15 @@ impl FeeEstimator for LampoChainManager<'_> {
 }
 
 /// Brodcaster Interface implementation for Lampo.
-impl BroadcasterInterface for LampoChainManager<'_> {
+impl BroadcasterInterface for LampoChainManager {
     fn broadcast_transaction(&self, tx: &Transaction) {
         self.backend.brodcast_tx(tx);
     }
 }
 
 // FIXME: todo implement it.
-impl Filter for LampoChainManager<'_> {
-    fn register_output(
-        &self,
-        output: lightning::chain::WatchedOutput,
-    ) -> Option<(usize, Transaction)> {
-        None
-    }
+impl Filter for LampoChainManager {
+    fn register_output(&self, output: lightning::chain::WatchedOutput) {}
 
     fn register_tx(&self, txid: &bitcoin::Txid, script_pubkey: &bitcoin::Script) {}
 }
