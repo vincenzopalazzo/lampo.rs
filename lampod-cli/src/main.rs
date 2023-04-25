eee17a20bbb362308147c07b431cb52638dda6ba
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-25 03:05:58 +0200

    feat(lampod-cli): remote the clap parser to use a smaller one
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampod-cli/src/main.rs
+++ lampod-cli/src/main.rs
@@ -7,7 +7,6 @@ use std::str::FromStr;
 use std::sync::Arc;
 use std::thread::JoinHandle;
 
-use clap::Parser;
 use log;
 use tokio::runtime::Runtime;
 
@@ -28,24 +27,20 @@ use crate::args::LampoCliArgs;
 #[tokio::main]
 async fn main() -> error::Result<()> {
     logger::init(log::Level::Info).expect("initializing logger for the first time");
-    let args = LampoCliArgs::parse();
+    let args = args::parse_args()?;
     run(args).await?;
     Ok(())
 }
 
 async fn run(args: LampoCliArgs) -> error::Result<()> {
-    let Some(path) = args.conf else {
-            error::bail!("Fails to parse the conf file at path {:?}", args.conf);
-    };
+    let path = args.conf;
     let mut lampo_conf = LampoConf::try_from(path)?;
 
-    if let Some(network_str) = args.network {
-        lampo_conf.set_network(&network_str)?;
-    };
+    lampo_conf.set_network(&args.network)?;
 
     let mut lampod = LampoDeamon::new(lampo_conf.clone());
     let keys = Arc::new(LampoKeys::new());
-    let client = match args.client.clone().unwrap().as_str() {
+    let client = match args.client.clone().as_str() {
         "nakamoto" => {
             let mut conf = Config::default();
             conf.network = Network::from_str(&lampo_conf.network.to_string()).unwrap();
