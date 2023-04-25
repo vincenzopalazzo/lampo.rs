8212fe8729e60fe924bd77b2aa121381c8aa4ddc
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-25 02:00:51 +0200

    feat(jsonrpc): move the context inside the handler
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampod/src/jsonrpc.rs
+++ lampod/src/jsonrpc.rs
@@ -2,21 +2,20 @@
 pub mod inventory;
 pub mod peer_control;
 
-use std::env;
+use std::sync::Arc;
 
-use lampo_client::UnixClient;
 use lampo_common::conf::LampoConf;
 use lampo_common::error;
 use lampo_common::json;
 use lampo_jsonrpc::command::Context;
 use lampo_jsonrpc::json_rpc2;
-use once_cell::sync::Lazy;
+use lampo_jsonrpc::Handler;
 
 use crate::{handler::external_handler::ExternalHandler, LampoDeamon};
 
 /// JSON RPC 2.0 Command handler!
 pub struct CommandHandler {
-    pub client: Lazy<UnixClient>,
+    pub handler: Option<Arc<Handler<LampoDeamon>>>,
     pub conf: LampoConf,
 }
 
@@ -25,12 +24,8 @@ unsafe impl Sync for CommandHandler {}
 
 impl CommandHandler {
     pub fn new(lampo_conf: &LampoConf) -> error::Result<Self> {
-        let client: Lazy<UnixClient> = Lazy::new(|| {
-            let path = env::var("LAMPO_UNIX").unwrap();
-            UnixClient::new(&path).unwrap()
-        });
         let handler = CommandHandler {
-            client,
+            handler: None,
             conf: lampo_conf.clone(),
         };
         Ok(handler)
@@ -39,13 +34,21 @@ impl CommandHandler {
 
 impl ExternalHandler for CommandHandler {
     fn handle(&self, req: &json_rpc2::Request<json::Value>) -> error::Result<Option<json::Value>> {
+        let Some(ref handler) = self.handler else {
+            log::info!("skipping the handling because it is not defined");
+            return Ok(None);
+        };
         log::debug!("handling the JSON RPC response with req {:?}", req);
-        let resp = self.client.call(&req.method, req.params.clone())?;
+        // FIXME: store the ctx inside the handler and not take as argument!
+        let Some(resp) = handler.run_callback(&req) else {
+            log::info!("callback `{}` not found, skipping handler", req.method);
+            return Ok(None);
+        };
         // FIXME: we should manage the handler when we try to handle
         // a method that it is not supported by this handler
         //
         // Like we should look at the error code, and return None.
-        Ok(resp)
+        Ok(Some(resp?))
     }
 }
 
