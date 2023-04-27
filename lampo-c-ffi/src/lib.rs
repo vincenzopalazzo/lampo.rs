f2a3769470a0ba3798fe8edd492559059c48e046
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-27 15:02:00 +0200

    feat(c-ffi): introduce the basic lampod API through C FFI
    This introduce the basic API to add the possibility to embedd
    the lampod code inside any language through the C FFI.
    
    This is an experimental version of the package because I do not
    have any experience in writing these kind of C FFI bindings.
    
    So all the work is based on [1] that make my life easy!
    
    [1] https://bheisler.github.io/post/calling-rust-in-python
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ lampo-c-ffi/src/lib.rs
@@ -0,0 +1,137 @@
+//! Exposing C FFI for interact with Lampo API
+//! and build easly a node.
+use std::sync::Arc;
+
+use libc;
+
+use lampo_common::conf::LampoConf;
+use lampod::LampoDeamon;
+
+#[macro_export]
+macro_rules! null {
+    () => {
+        std::ptr::null_mut()
+    };
+}
+
+#[macro_export]
+macro_rules! from_cstr {
+    ($x:expr) => {{
+        use std::ffi::CStr;
+        let c_str = unsafe { CStr::from_ptr($x) };
+        let Ok(c_str) = c_str.to_str() else {
+                                                                        return null!()
+                                                                    };
+        c_str
+    }};
+}
+
+#[macro_export]
+macro_rules! json_buffer {
+    ($x:expr) => {{
+        use lampo_common::json;
+        let Ok(buff) = json::to_string_pretty($x) else {
+                                                                        return null!();
+                                                                    };
+        buff.as_ptr()
+    }};
+}
+
+#[macro_export]
+macro_rules! c_free {
+    ($x:expr) => {{
+        if !$x.is_null() {
+            unsafe { Box::from_raw($x) };
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! as_rust {
+    ($x:expr) => {{
+        if !$x.is_null() {
+            unsafe { Some(Arc::from_raw($x)) }
+        } else {
+            None
+        }
+    }};
+}
+
+/// Allow to create a lampo deamon from a configuration patch!
+pub extern "C" fn new_lampod(conf_path: *const libc::c_char) -> *mut LampoDeamon {
+    use lampod::chain::{LampoWalletManager, WalletManager};
+    let conf_path_t = from_cstr!(conf_path);
+    let conf = match LampoConf::try_from(conf_path_t.to_owned()) {
+        Ok(conf) => conf,
+        // FIXME: log the error!
+        Err(_) => return null!(),
+    };
+    let Ok(wallet) = LampoWalletManager::new(conf.network) else {
+        return null!();
+    };
+    let lampod = Box::new(LampoDeamon::new(conf, Arc::new(wallet)));
+    Box::into_raw(lampod)
+}
+
+/// Add a JSON RPC 2.0 Sever that listen on a unixsocket, and return a error code
+/// < 0 is an error happens, or 0 is all goes well.
+pub extern "C" fn add_jsonrpc_on_unixsocket(lampod: *mut LampoDeamon) -> i64 {
+    use lampo_jsonrpc::JSONRPCv2;
+    use lampod::jsonrpc::inventory::get_info;
+    use lampod::jsonrpc::open_channel::json_open_channel;
+    use lampod::jsonrpc::peer_control::json_connect;
+    use lampod::jsonrpc::CommandHandler;
+
+    let Some(lampod) = as_rust!(lampod) else {
+        return -1;
+    };
+    let socket_path = format!("{}/lampod.socket", lampod.root_path());
+    std::env::set_var("LAMPO_UNIX", socket_path.clone());
+    let Ok(server) = JSONRPCv2::new(lampod.clone(), &socket_path) else {
+        return -2;
+    };
+    server.add_rpc("getinfo", get_info).unwrap();
+    server.add_rpc("connect", json_connect).unwrap();
+    server.add_rpc("fundchannel", json_open_channel).unwrap();
+    let rpc_handler = server.handler();
+    let Ok(lampo_handler) = CommandHandler::new(lampod.conf()) else {
+        return -2;
+    };
+    lampo_handler.set_handler(rpc_handler);
+    let lampo_handler = Arc::new(lampo_handler);
+    let Ok(()) = lampod.add_external_handler(lampo_handler.clone()) else {
+        return -2;
+    };
+
+    // FIXME: this is blocking?
+    let _ = server.spawn().join();
+    0
+}
+
+pub extern "C" fn lampod_call(
+    lampod: *mut LampoDeamon,
+    method: *const libc::c_char,
+    buffer: *const libc::c_char,
+) -> *const u8 {
+    use lampo_common::json;
+
+    let Some(lampod) = as_rust!(lampod) else {
+        return null!();
+    };
+    let method = from_cstr!(method);
+    let buffer = from_cstr!(buffer);
+    let Ok(payload) = json::from_str::<json::Value>(buffer) else {
+        return null!();
+    };
+    let response = lampod.call(method, payload);
+    // FIXME Encode this to a string
+    match response {
+        Ok(resp) => json_buffer!(&resp),
+        Err(_) => null!(),
+    }
+}
+
+/// Allow to create a lampo deamon from a configuration patch!
+pub extern "C" fn free_lampod(lampod: *mut LampoDeamon) {
+    c_free!(lampod);
+}
