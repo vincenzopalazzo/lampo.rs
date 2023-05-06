d02541e8e50b04c9a29b8cb111a3e8a575268a5c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-06 23:54:33 +0200

    feat(binding): support the first version for dart
    This commit suppor a first simple version for the dart binding
    for dart.
    
    The next step is support flutter, but for now with dart
    we should install the `.so` library in the sysytem.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampo-lang-bind/lampo_dart/lib/src/lampo_dart_base.dart
+++ lampo-lang-bind/lampo_dart/lib/src/lampo_dart_base.dart
@@ -1,44 +1,32 @@
 import 'dart:convert';
 import 'dart:ffi';
-import 'dart:html';
-import 'dart:isolate';
 
 import 'package:ffi/ffi.dart';
 
 import 'package:lampo_dart/src/ffi/generated_bindings.dart';
 
-final ffi = LampoFFI(DynamicLibrary.open("/usr/local/lib/liblampo_lib.so"));
+final ffi = LampoFFI(DynamicLibrary.open("/usr/local/lib/liblampo.so"));
 
 /// Lampo Class Implementation
 class Lampo {
   late Pointer<LampoDeamon> inner;
-  ReceivePort? chan;
 
   Lampo({required String homePath}) {
     inner = ffi.new_lampod(homePath.toNativeUtf8().cast<Int8>());
   }
 
-  Future<void> spawn() async {
-    chan = ReceivePort();
+  void spawn() {
     ffi.add_jsonrpc_on_unixsocket(inner);
     ffi.lampo_listen(inner);
-    return Future.delayed(Duration(milliseconds: 10), () async {
-        await Isolate.spawn((sendChan) {
-        ffi.lampo_listen(inner);
-        sendChan.send(true);
-      },
-      chan!.sendPort);
-});
   }
 
   void stop() {
-    chan!.listen((message) { });
     ffi.free_lampod(inner);
   }
 
   Future<Map<String, dynamic>> _call({required String method, Map<String, dynamic> payload = const {}}) async {
     var jsonStr = json.encode(payload);
-    var response = ffi.lampod_call(inner, method.toNativeUtf8().cast(), jsonStr.toNativeUtf8().cast()).toString();
+    var response = ffi.lampod_call(inner, method.toNativeUtf8().cast(), jsonStr.toNativeUtf8().cast()).cast<Utf8>().toDartString();
     return json.decode(response);
   }
 
@@ -49,6 +37,6 @@ class Lampo {
   /// In case you are running the application as command line, so there is no
   /// problem, and this will be has only benefits (maybe, if too many isolate are spawn?).
   Future<Map<String, dynamic>> call({required String method, Map<String, dynamic> payload = const {}}) async {
-    return await Isolate.run(() => _call(method: method, payload: payload));
+    return await _call(method: method, payload: payload);
   }
 }
--- lampo-lang-bind/lampo_dart/test/lampo_dart_test.dart
+++ lampo-lang-bind/lampo_dart/test/lampo_dart_test.dart
@@ -1,17 +1,21 @@
 import 'dart:io';
 
 import 'package:test/test.dart';
+import 'package:logger/logger.dart';
 
 import 'package:lampo_dart/lampo_dart.dart';
 
 void main() {
   group('Run the lampo node from dart', () {
+      var logger = Logger();
 
       test("init lampo node and call the `getinfo`", () async {
           var lampo = Lampo(homePath: "/home/vincent/.lampo/testnet/");
-          await lampo.spawn();
+          lampo.spawn();
+          // Wait that the node wills start
+          sleep(Duration(seconds: 2));
           var info = await lampo.call(method: "getinfo");
-          print(info);
+          logger.d(info);
           lampo.stop();
       });
 
