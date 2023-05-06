74ca4b5630f589f127a1b97242e433df723eeea7
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-07 01:49:04 +0200

    feat(dart-binding): support android
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampo-lang-bind/lampo_dart/test/lampo_dart_test.dart
+++ lampo-lang-bind/lampo_dart/test/lampo_dart_test.dart
@@ -7,17 +7,16 @@ import 'package:lampo_dart/lampo_dart.dart';
 
 void main() {
   group('Run the lampo node from dart', () {
-      var logger = Logger();
+    var logger = Logger();
 
-      test("init lampo node and call the `getinfo`", () async {
-          var lampo = Lampo(homePath: "/home/vincent/.lampo/testnet/");
-          lampo.spawn();
-          // Wait that the node wills start
-          sleep(Duration(seconds: 2));
-          var info = await lampo.call(method: "getinfo");
-          logger.d(info);
-          lampo.stop();
-      });
-
+    test("init lampo node and call the `getinfo`", () async {
+      var lampo = Lampo(homePath: "/home/vincent/.lampo/testnet/");
+      lampo.spawn();
+      // Wait that the node wills start
+      sleep(Duration(seconds: 2));
+      var info = await lampo.call(method: "getinfo");
+      logger.d(info);
+      lampo.stop();
+    });
   });
 }
