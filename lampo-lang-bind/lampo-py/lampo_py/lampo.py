4ce81d75337c046a387ee0909cb4b084cada9e2f
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-05-23 21:50:26 +0200

    feat(lampo-py): update the python binding with the last ffi version
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampo-lang-bind/lampo-py/lampo_py/lampo.py
+++ lampo-lang-bind/lampo-py/lampo_py/lampo.py
@@ -4,6 +4,17 @@ ffi = FFI()
 
 ffi.cdef(
     """
+    /**
+    * LampoDaemon is the main data structure that uses the facade
+    * pattern to hide the complexity of the LDK library. You can interact
+    * with the LampoDaemon's components through access
+    * methods (similar to get methods in modern procedural languages).
+    *
+    * Another way to view the LampoDaemon is as
+    * a microkernel pattern, especially for developers
+    * who are interested in building their own node on
+    * top of the LampoDaemon.
+    */
     typedef struct LampoDeamon LampoDeamon;
 
     /**
@@ -17,18 +28,27 @@ ffi.cdef(
     */
     void free_lampod(struct LampoDeamon *lampod);
 
+    /**
+    * Add a JSON RPC 2.0 Sever that listen on a unixsocket, and return a error code
+    * < 0 is an error happens, or 0 is all goes well.
+    */
+    const char *lampo_last_errror(void);
+
+    /**
+    * Allow to create a lampo deamon from a configuration patch!
+    */
+    void lampo_listen(struct LampoDeamon *lampod);
+
     const char *lampod_call(struct LampoDeamon *lampod, const char *method, const char *buffer);
 
     /**
     * Allow to create a lampo deamon from a configuration patch!
     */
     struct LampoDeamon *new_lampod(const char *conf_path);
-
-    void lampo_listen(struct LampoDeamon *lampod);
 """
 )
 
-lampod = ffi.dlopen("/usr/local/lib/liblampo_lib.so")
+lampod = ffi.dlopen("/usr/local/lib/liblampo.so")
 
 import json
 import logging
