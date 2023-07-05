vincenzopalazzo/lampo.rs

commit d672929d75f76bf713438e631a9b28418dedf505

fix(lampod): Adds event emission
Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

lampo-bitcoind/src/lib.rs
@@ -1,20 +1,26 @@
 //! Implementation of the bitcoin backend for
 //! lampo.
+use std::borrow::Borrow;
+use std::cell::Cell;
+use std::cell::RefCell;
+use std::sync::Arc;
+
 use bitcoincore_rpc::Client;
 use bitcoincore_rpc::Result;
 use bitcoincore_rpc::RpcApi;
 
 use bitcoincore_rpc::bitcoin::hashes::Hash;
-use bitcoincore_rpc::RawTx;
 use lampo_common::backend::Backend;
 use lampo_common::backend::BlockHash;
 use lampo_common::backend::BlockSourceResult;
 use lampo_common::backend::{deserialize, serialize};
 use lampo_common::backend::{Block, BlockData};
+use lampo_common::handler::Handler;
 use lampo_common::secp256k1::hashes::hex::ToHex;
 
 pub struct BitcoinCore {
     inner: Client,
+    handler: RefCell<Option<Arc<dyn Handler>>>,
 }
 
 impl BitcoinCore {
@@ -23,6 +29,7 @@ impl BitcoinCore {
         use bitcoincore_rpc::Auth;
         Ok(Self {
             inner: Client::new(url, Auth::UserPass(user.to_owned(), pass.to_owned()))?,
+            handler: RefCell::new(None),
         })
     }
 }
@@ -75,12 +82,16 @@ impl Backend for BitcoinCore {
         use bitcoincore_rpc::bitcoin::BlockHash;
         // FIXME: add in bitcoin core the from method
         use bitcoincore_rpc::bitcoin::consensus::serialize as inner_serialize;
+        use lampo_common::event::onchain::OnChainEvent;
+        use lampo_common::event::Event;
 
         // FIXME: change the version of rust bitcoin in nakamoto and in lampod_common.
         let bytes = serialize(header_hash);
         let hash = BlockHash::from_slice(bytes.as_slice()).unwrap();
         let result = self.inner.get_block(&hash).unwrap();
         let block: Block = deserialize(&inner_serialize(&result)).unwrap();
+        let handler = self.handler.borrow().clone().unwrap();
+        handler.emit(Event::OnChain(OnChainEvent::NewBlock(block.clone())));
 
         sync! {
            Ok(BlockData::FullBlock(block))
@@ -121,5 +132,9 @@ impl Backend for BitcoinCore {
         _script: &lampo_common::backend::Script,
     ) {
         unimplemented!()
+    }
+
+    fn set_handler(&self, handler: Arc<dyn Handler>) {
+        self.handler.replace(Some(handler));
     }
 }

lampo-nakamoto/src/lib.rs
@@ -1,7 +1,9 @@
 //! Nakamoto backend implementation for Lampo
 use std::cell::Cell;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::net::TcpStream;
+use std::sync::Arc;
 
 use esplora_client::BlockingClient;
 use esplora_client::Builder;
@@ -20,12 +22,16 @@ use lampo_common::backend::BlockHeaderData;
 use lampo_common::backend::UtxoResult;
 use lampo_common::backend::WatchedOutput;
 use lampo_common::error;
+use lampo_common::event::onchain::OnChainEvent;
+use lampo_common::event::Event;
+use lampo_common::handler::Handler;
 
 #[derive(Clone)]
 pub struct Nakamoto {
-    handler: nakamoto_client::Handle<Waker>,
+    nakamoto: nakamoto_client::Handle<Waker>,
     current_height: Cell<Option<Height>>,
     rest: BlockingClient,
+    handler: RefCell<Option<Arc<dyn Handler>>>,
 }
 
 impl Nakamoto {
@@ -46,11 +52,12 @@ impl Nakamoto {
         // FIXME: join this later
         let _worker = std::thread::spawn(|| nakamoto.run(config));
         let client = Nakamoto {
-            handler,
+            nakamoto: handler,
             current_height: Cell::new(None),
             rest: Builder::new(url)
                 .build_blocking()
                 .map_err(|err| error::anyhow!("{err}"))?,
+            handler: RefCell::new(None),
         };
         Ok(client)
     }
@@ -78,11 +85,17 @@ impl Backend for Nakamoto {
         &'a self,
         header_hash: &'a nakamoto_common::block::BlockHash,
     ) -> AsyncBlockSourceResult<'a, BlockData> {
-        let Some(block) = self.handler.get_block(header_hash).unwrap() else {
+        let blk_chan = self.nakamoto.blocks();
+        let Some(block) = self.nakamoto.get_block(header_hash).unwrap() else {
             unimplemented!();
         };
         log::info!("get block information {:?}", block);
         self.current_height.set(Some(block.0));
+
+        let handler = self.handler.borrow().clone().unwrap();
+        let (blk, _) = blk_chan.recv().unwrap();
+        handler.emit(Event::OnChain(OnChainEvent::NewBlock(blk)));
+
         sync! { Ok(BlockData::HeaderOnly(block.1)) }
     }
 
@@ -99,10 +112,13 @@ impl Backend for Nakamoto {
     }
 
     fn brodcast_tx(&self, tx: &nakamoto_common::block::Transaction) {
-        let result = self.handler.submit_transaction(tx.clone());
+        let result = self.nakamoto.submit_transaction(tx.clone());
         if let Err(err) = result {
             log::error!("brodcast tx fails: {err}");
-        };
+        } else {
+            let handler = self.handler.borrow().clone().unwrap();
+            handler.emit(Event::OnChain(OnChainEvent::SendRawTransaction(tx.clone())));
+        }
     }
 
     fn is_lightway(&self) -> bool {
@@ -112,7 +128,7 @@ impl Backend for Nakamoto {
     fn get_best_block<'a>(
         &'a self,
     ) -> AsyncBlockSourceResult<(nakamoto_common::block::BlockHash, Option<u32>)> {
-        let tip = self.handler.get_tip().unwrap();
+        let tip = self.nakamoto.get_tip().unwrap();
         sync! { Ok((tip.blk_header.block_hash(), Some(tip.height as u32))) }
     }
 
@@ -130,5 +146,9 @@ impl Backend for Nakamoto {
 
     fn get_utxo(&self, block: &BlockHash, idx: u64) -> UtxoResult {
         todo!()
+    }
+
+    fn set_handler(&self, handler: std::sync::Arc<dyn lampo_common::handler::Handler>) {
+        self.handler.replace(Some(handler));
     }
 }

lampo-common/src/lib.rs
@@ -1,10 +1,16 @@
 pub mod backend;
 pub mod chacha20;
 pub mod conf;
+pub mod event;
+pub mod handler;
 pub mod keymanager;
 pub mod logger;
 pub mod model;
 pub mod types;
+
+pub mod ldk {
+    pub use lightning::*;
+}
 
 pub mod error {
     pub use anyhow::*;
@@ -12,6 +18,10 @@ pub mod error {
 
 pub mod json {
     pub use serde_json::*;
+}
+
+pub mod chan {
+    pub use crossbeam_channel::*;
 }
 
 pub use bitcoin;

lampod/src/lib.rs
@@ -13,7 +13,7 @@
 pub mod actions;
 mod builtin;
 pub mod chain;
-pub mod events;
+pub mod command;
 pub mod handler;
 pub mod jsonrpc;
 pub mod keys;
@@ -41,7 +41,7 @@ use lampo_jsonrpc::json_rpc2::Request;
 use crate::actions::handler::LampoHandler;
 use crate::actions::Handler;
 use crate::chain::LampoChainManager;
-use crate::events::LampoEvent;
+use crate::command::Command;
 use crate::ln::{LampoChannelManager, LampoInventoryManager, LampoPeerManager};
 use crate::persistence::LampoPersistence;
 use crate::utils::logger::LampoLogger;
@@ -192,6 +192,7 @@ impl LampoDeamon {
         self.init_peer_manager()?;
         self.init_inventory_manager()?;
         self.init_event_handler()?;
+        client.set_handler(self.handler());
         Ok(())
     }
 
@@ -255,7 +256,7 @@ impl LampoDeamon {
         // the handler down different method.
         let request = Request::new(method, args);
         let (sender, receiver) = chan::bounded::<json::Value>(1);
-        let command = LampoEvent::from_req(&request, &sender)?;
+        let command = Command::from_req(&request, &sender)?;
         log::info!("received {:?}", command);
         let Some(ref handler) = self.handler else {
             error::bail!("at this point the handler should be not None");

lampo-common/Cargo.toml
@@ -9,6 +9,7 @@ edition = "2021"
 lightning = { git = "https://github.com/lightningdevkit/rust-lightning.git", features = ["max_level_trace"] }
 lightning-block-sync = { git = "https://github.com/lightningdevkit/rust-lightning.git" }
 clightningrpc-conf = { git = "https://github.com/laanwj/cln4rust.git", branch = "master" }
+crossbeam-channel = "0.5.8"
 bitcoin = "0.29.0"
 anyhow = "1.0.70"
 colored = "1.9"

lampo-common/src/backend.rs
@@ -1,6 +1,8 @@
 //! ...
 //! Beckend implementation
 
+use std::sync::Arc;
+
 pub use bitcoin::consensus::{deserialize, serialize};
 pub use bitcoin::{Block, BlockHash, Script, Transaction, Txid};
 pub use lightning::chain::WatchedOutput;
@@ -8,6 +10,8 @@ pub use lightning::routing::utxo::UtxoResult;
 pub use lightning_block_sync::{
     AsyncBlockSourceResult, BlockData, BlockHeaderData, BlockSourceResult,
 };
+
+use crate::handler::Handler;
 
 /// Bakend Trait specification
 pub trait Backend {
@@ -40,4 +44,6 @@ pub trait Backend {
     fn get_best_block<'a>(&'a self) -> AsyncBlockSourceResult<(BlockHash, Option<u32>)>;
 
     fn get_utxo(&self, block: &BlockHash, idx: u64) -> UtxoResult;
+
+    fn set_handler(&self, _: Arc<dyn Handler>) {}
 }

lampo-common/src/event.rs
@@ -1,0 +1,68 @@
+//! Events commands
+pub mod ln;
+pub mod onchain;
+
+use std::sync::{Arc, Mutex};
+
+use crate::chan;
+use crate::event::ln::LightningEvent;
+use crate::event::onchain::OnChainEvent;
+
+/// Publishes events to subscribers.
+#[derive(Clone)]
+pub struct Emitter<T> {
+    subscribers: Arc<Mutex<Vec<chan::Sender<T>>>>,
+}
+
+impl<T> Default for Emitter<T> {
+    fn default() -> Self {
+        Self {
+            subscribers: Default::default(),
+        }
+    }
+}
+
+impl<T: Clone> Emitter<T> {
+    /// Emit an event to all subscribers and drop subscribers who can't receive it.
+    pub fn emit(&self, event: T) {
+        self.subscribers
+            .lock()
+            .unwrap()
+            .retain(|s| s.try_send(event.clone()).is_ok());
+    }
+
+    /// Drop all subscribers.
+    pub fn close(self) {
+        self.subscribers.lock().unwrap().clear();
+    }
+
+    /// Create a subscriber from this emitter.
+    pub fn subscriber(&self) -> Subscriber<T> {
+        Subscriber {
+            subscribers: self.subscribers.clone(),
+        }
+    }
+}
+
+/// Subscribes to events.
+#[derive(Clone)]
+pub struct Subscriber<T> {
+    subscribers: Arc<Mutex<Vec<chan::Sender<T>>>>,
+}
+
+impl<T: Clone> Subscriber<T> {
+    /// Add a subscription to receive broadcast events.
+    pub fn subscribe(&self) -> chan::Receiver<T> {
+        let (sender, receiver) = chan::unbounded();
+        let mut subs = self.subscribers.lock().unwrap();
+        subs.push(sender);
+        receiver
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    Lightning(LightningEvent),
+    OnChain(OnChainEvent),
+    Inventory,
+}

lampo-common/src/event/ln.rs
@@ -1,0 +1,27 @@
+use crate::bitcoin::Transaction;
+use crate::ldk::ln::features::ChannelTypeFeatures;
+use crate::types::NodeId;
+
+#[derive(Clone, Debug)]
+pub enum LightningEvent {
+    // FIXME: add new peer model
+    PeerConnect {
+        counterparty_node_id: NodeId,
+    },
+    ChannelReady {
+        counterparty_node_id: NodeId,
+        channel_id: [u8; 32],
+        channel_type: ChannelTypeFeatures,
+    },
+    FundingChannelStart {
+        counterparty_node_id: NodeId,
+        temporary_channel_id: [u8; 32],
+        channel_value_satoshis: u64,
+    },
+    FundingChannelEnd {
+        counterparty_node_id: NodeId,
+        temporary_channel_id: [u8; 32],
+        channel_value_satoshis: u64,
+        funding_transaction: Transaction,
+    },
+}

commit c6d4ffe0ea90dafde52ada71a58efdf108d0a946

fix(nakamoto): fixes test crash when handler in None
This commit fix a bug that was introduced while adding
events to lampod.

Fixes: d672929d
Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

lampo-bitcoind/src/lib.rs
@@ -1,7 +1,5 @@
 //! Implementation of the bitcoin backend for
 //! lampo.
-use std::borrow::Borrow;
-use std::cell::Cell;
 use std::cell::RefCell;
 use std::sync::Arc;
 
@@ -90,8 +88,10 @@ impl Backend for BitcoinCore {
         let hash = BlockHash::from_slice(bytes.as_slice()).unwrap();
         let result = self.inner.get_block(&hash).unwrap();
         let block: Block = deserialize(&inner_serialize(&result)).unwrap();
-        let handler = self.handler.borrow().clone().unwrap();
-        handler.emit(Event::OnChain(OnChainEvent::NewBlock(block.clone())));
+        let _ = self.handler.borrow().clone().and_then(|handler| {
+            handler.emit(Event::OnChain(OnChainEvent::NewBlock(block.clone())));
+            Some(handler)
+        });
 
         sync! {
            Ok(BlockData::FullBlock(block))

lampo-nakamoto/src/lib.rs
@@ -92,10 +92,11 @@ impl Backend for Nakamoto {
         log::info!("get block information {:?}", block);
         self.current_height.set(Some(block.0));
 
-        let handler = self.handler.borrow().clone().unwrap();
-        let (blk, _) = blk_chan.recv().unwrap();
-        handler.emit(Event::OnChain(OnChainEvent::NewBlock(blk)));
-
+        let _ = self.handler.borrow().clone().and_then(|handler| {
+            let (blk, _) = blk_chan.recv().unwrap();
+            handler.emit(Event::OnChain(OnChainEvent::NewBlock(blk)));
+            Some(handler)
+        });
         sync! { Ok(BlockData::HeaderOnly(block.1)) }
     }
 