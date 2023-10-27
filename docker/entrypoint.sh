7555c1d68a5717f55137c17d9025c630f9e4b371
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-10-27 22:32:11 +0200

    ci: fix lnprototest CI
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [[ -z "${PROTO_TEST}" ]]; then
+if [ "$PROTO_TEST" -eq 1 ]; then
+    echo "running lnprototest"
+    make 
+    sudo cp target/debug/liblampo.so /usr/local/lib
     pip3 install poetry
-    cd tests/lnprototest; poetry install && poetry run "make check" 
+    cd tests/lnprototest; poetry install && poetry run make check 
 else
+    echo "running integration tests"
     RUST_BACKTRACE=full make integration
 fi
