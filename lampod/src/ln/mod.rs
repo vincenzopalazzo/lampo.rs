0571b1454457a721fea708de255d974d521958c4
Elias Rohrer <dev@tnull.de>
2024-01-02 09:01:42 +0100

    Rename `channe_manager.rs` to `channel_manager.rs`
    ... as this seems to be an obvious typo.

--- lampod/src/ln/mod.rs
+++ lampod/src/ln/mod.rs
@@ -1,5 +1,5 @@
 //! Lampo Channel Manager
-mod channe_manager;
+mod channel_manager;
 mod inventory_manager;
 mod offchain_manager;
 mod peer_manager;
@@ -7,7 +7,7 @@ mod peer_manager;
 pub mod events;
 pub mod peer_event;
 
-pub use channe_manager::LampoChannelManager;
+pub use channel_manager::LampoChannelManager;
 pub use inventory_manager::LampoInventoryManager;
 pub use offchain_manager::OffchainManager;
 pub use peer_manager::LampoPeerManager;
