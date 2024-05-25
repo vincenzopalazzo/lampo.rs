a6dfe907097489417d5ce310ef5fdbed070d2542
Harshit Verma <harshitverma933@gmail.com>
2024-05-25 05:47:11 +0530

    feat(getinfo) : specify address inside getinfo
    This commit adds the address inside `getinfo` command.
    Changelog:
    - Adds `NetworkInfo` response inside lampo_common::model

--- lampo-common/src/model/getinfo.rs
+++ lampo-common/src/model/getinfo.rs
@@ -9,4 +9,11 @@ pub struct GetInfo {
     pub alias: String,
     pub blockheight: u32,
     pub lampo_dir: String,
+    pub address: Vec<NetworkInfo>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NetworkInfo {
+    pub address: String,
+    pub port: u64,
 }
