a90daf4708c78f34315afe91d48f3e0d2471f879
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-06-04 01:20:56 +0200

    ci: run the ci with the nix
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-echo "lnprototest enabled: $PROTO_TEST"
-if [ "$PROTO_TEST" -eq 1 ]; then
-    echo "running lnprototest"
-    make 
-    cp target/debug/liblampo.so /usr/local/lib
-    pip3 install poetry
-    cd tests/lnprototest; poetry install && poetry run make check 
-else
-    echo "running integration tests"
-    RUST_BACKTRACE=full make integration
-fi
+echo "running lnprototest"
+make 
+pip3 install poetry
+cd tests/lnprototest; poetry install && poetry run make check 
