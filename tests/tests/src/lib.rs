7b293fcdcc28f72617b1be644c4b8384dddbd87b
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-06-08 21:11:44 +0200

    tests: allow to customize the log level
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- tests/tests/src/lib.rs
+++ tests/tests/src/lib.rs
@@ -13,10 +13,18 @@ static INIT: Once = Once::new();
 
 #[cfg(test)]
 fn init() {
+    let level = std::env::var("TEST_LOG_LEVEL");
     // ignore error
-    INIT.call_once(|| {
-        use lampo_common::logger;
-
-        logger::init("trace", None).expect("initializing logger for the first time");
+    INIT.call_once(|| match level {
+        Ok(level) => {
+            if let Err(e) = lampo_common::logger::init(&level, None) {
+                eprintln!("Error initializing logger: {}", e);
+            } else {
+                println!("Logger initialized with level: {}", level);
+            }
+        }
+        Err(e) => {
+            eprintln!("Error reading TEST_LOG_LEVEL: {}", e);
+        }
     });
 }
