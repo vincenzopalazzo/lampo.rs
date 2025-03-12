c015bb2e4538ff302f7ad360c3fac89bd2ec808c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2025-03-12 17:08:40 +0100

    nix: upgrade nix build
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampo-httpd/src/handler.rs
+++ lampo-httpd/src/handler.rs
@@ -1,3 +1,6 @@
+use std::io::Cursor;
+
+use elite_rpc::protocol::Protocol;
 use elite_rpc::transport::curl::HttpTransport;
 use elite_rpc::transport::TransportMethod;
 use elite_rpc::EliteRPC;
@@ -6,8 +9,6 @@ use lampo_common::error;
 use lampo_common::handler::ExternalHandler;
 use lampo_common::json;
 use lampo_common::jsonrpc::Request;
-
-use crate::rest_protocol::RestProtocol;
 
 pub struct HttpdHandler {
     inner: EliteRPC<HttpTransport<RestProtocol>, RestProtocol>,
@@ -26,5 +27,34 @@ impl ExternalHandler for HttpdHandler {
         let body = req.params.clone();
         let response = self.inner.call(TransportMethod::Post(method), &body)?;
         Ok(Some(response))
+    }
+}
+
+#[derive(Clone)]
+pub struct RestProtocol;
+
+impl Protocol for RestProtocol {
+    type InnerType = json::Value;
+
+    fn new() -> error::Result<Self> {
+        Ok(Self)
+    }
+
+    fn to_request(
+        &self,
+        url: &str,
+        req: &Self::InnerType,
+    ) -> error::Result<(String, Self::InnerType)> {
+        Ok((url.to_string(), req.clone()))
+    }
+
+    fn from_request(
+        &self,
+        content: &[u8],
+        _: std::option::Option<elite_rpc::protocol::Encoding>,
+    ) -> error::Result<<Self as elite_rpc::protocol::Protocol>::InnerType> {
+        let cursor = Cursor::new(content);
+        let response: json::Value = json::from_reader(cursor)?;
+        Ok(response)
     }
 }
