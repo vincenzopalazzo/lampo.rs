9e6cd1a3b25d4c1bf044adb33f946cecce6db64e
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2025-06-06 09:38:20 +0200

    common: use a custom for for custom transactions
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- lampo-common/src/keys.rs
+++ lampo-common/src/keys.rs
@@ -1,9 +1,9 @@
 use std::{sync::Arc, time::SystemTime};
 
 use bitcoin::secp256k1::{Secp256k1, SecretKey};
-use lightning::bolt11_invoice;
 use lightning::sign::{InMemorySigner, NodeSigner, OutputSpender, SignerProvider};
 
+use crate::ldk::invoice;
 use crate::ldk::sign::{EntropySource, KeysManager};
 
 /// Lampo keys implementations
@@ -146,7 +146,7 @@ impl NodeSigner for LampoKeysManager {
 
     fn sign_invoice(
         &self,
-        invoice: &bolt11_invoice::RawBolt11Invoice,
+        invoice: &invoice::RawBolt11Invoice,
         recipient: lightning::sign::Recipient,
     ) -> Result<bitcoin::secp256k1::ecdsa::RecoverableSignature, ()> {
         self.inner.sign_invoice(invoice, recipient)
